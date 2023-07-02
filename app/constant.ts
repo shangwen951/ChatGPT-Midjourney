e3764acadc7ea419faea688b05f96e2ad97d2c2d
shangwen951 <136416250+shangwen951@users.noreply.github.com>
2023-07-02 18:43:12 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,7 +1,7 @@
 export const OWNER = "Licoy";
-export const REPO = "ChatGPT-Midjourney";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
-export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
+export const REPO = "旷合AI";
+export const REPO_URL = `https://1github.com/${OWNER}/${REPO}`;
+export const ISSUE_URL = `https://1github.com/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
 export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
